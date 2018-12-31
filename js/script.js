0359aa6cdc4490b5f56bcd11053643fb5171093f
hussain <m.hussainul.islam@gmail.com>
2018-12-31 18:57:57 -0500

    added power, square root, square, percent, and reset buttons

--- js/script.js
+++ js/script.js
@@ -4,6 +4,10 @@ var display = document.querySelector("#display");
 var operations = document.querySelector("#operations");
 var clear = document.querySelector("#button_clr");
 var equal = document.querySelector("#button_eql");
+var sqrt = document.querySelector("#button_sqrt");
+var reset = document.querySelector("#button_reset");
+var square = document.querySelector("#button_square");
+var percent = document.querySelector("#button_percent");
 
 //Declaring global variables
 var displayNumber = "";
@@ -15,46 +19,123 @@ inputNumber.addEventListener('click', numberPress);
 operations.addEventListener('click', operationsPress);
 clear.addEventListener('click', inputClear);
 equal.addEventListener('click', operationEqual);
+sqrt.addEventListener('click', operationSQRT);
+reset.addEventListener('click', resetAll);
+square.addEventListener('click', operationSquare);
+percent.addEventListener('click', operationPercent);
 
 function numberPress(event){
     if(event.target.type === "button"){
-        displayNumber += event.target.value;
-        display.value = displayNumber;
+        display.value += event.target.value;
+        //display.value = displayNumber;
     }
 }
 
 function operationsPress(event){
     if(numberOne == null){
-        numberOne = parseInt(display.value);
+        numberOne = parseFloat(display.value);
         inputClear();
         displayNumber = "";
         opertionSymbol = event.target.value;
-        console.log("first: ", numberOne);
+        //console.log("first: ", numberOne);
     } else {
         operation(opertionSymbol);
+        if(isNaN(numberOne)){
+            alert("This is not a valid operation");
+            resetAll();
+        }
+        else{
+            display.value = numberOne;
+            displayNumber = "";
+            opertionSymbol = event.target.value;
+            //console.log("repeat: ", numberOne);
+        }
+    }
+}
+
+function operationSQRT(){
+    if(numberOne === null && display.value === ''){
+        alert("Please input a number first!");
+        resetAll();
+    }
+    else if(numberOne === null && display.value !== ''){
+        numberOne = Math.sqrt(parseFloat(display.value));
         display.value = numberOne;
+        numberOne = null;
+    }
+    else if(numberOne !== null && display.value === ''){
+        numberOne = Math.sqrt(numberOne);
+        display.value = numberOne;
+        numberOne = null;
+    }
+    else{
+        numberOne = Math.sqrt(parseFloat(display.value));
+        display.value = numberOne;
+        numberOne = null;
+    }
+}
+
+function operationSquare(){
+    if(numberOne === null && display.value === ''){
+        alert("Please input a number first!");
+        resetAll();
+    }
+    else if(numberOne === null && display.value !== ''){
+        numberOne = Math.pow(parseFloat(display.value),2);
+        display.value = numberOne;
+        numberOne = null;
+    }
+    else if(numberOne !== null && display.value === ''){
+        numberOne = Math.pow(numberOne,2);
+        display.value = numberOne;
+        numberOne = null;
+    }
+    else{
+        numberOne = Math.pow(parseFloat(display.value),2);
+        display.value = numberOne;
+        numberOne = null;
+    }
+}
+
+function operationPercent(){
+    display.value = parseFloat(display.value) / 100;
+}
+
+//Function when the equal sign is pressed
+function operationEqual() {
+    if(numberOne == null){
+        alert(`To press "Equal" you need to enter at least two numbers first.`);
+    }
+    else{
+        operation(opertionSymbol);
+        display.value = numberOne;
+        //console.log("Equal: ", numberOne);
+        numberOne = null;
         displayNumber = "";
-        opertionSymbol = event.target.value;
-        console.log("repeat: ", numberOne);
+        operationSymbol = "";
     }
 }
 
 function operation(type){
     switch(type){
         case '+':
-        numberOne += parseInt(display.value);
+        numberOne += parseFloat(display.value);
         break;
 
         case '-':
-        numberOne -= parseInt(display.value);
+        numberOne -= parseFloat(display.value);
         break;
 
         case 'x':
-        numberOne *= parseInt(display.value);
+        numberOne *= parseFloat(display.value);
         break;
 
         case '/':
-        numberOne /= parseInt(display.value);
+        numberOne /= parseFloat(display.value);
+        break;
+
+        case '^':
+        operationPower();
         break;
 
         default:
@@ -63,20 +144,15 @@ function operation(type){
     }
 }
 
+function operationPower(){
+    numberOne = Math.pow(numberOne, parseFloat(display.value));
+}
+
+//Clearing the input display field
 function inputClear(){
     display.value = "";
 }
 
-function operationEqual() {
-    if(numberOne == null){
-        alert(`To press "Equal" you need to enter at least two numbers first.`);
-    }
-    else{
-        operation(opertionSymbol);
-        display.value = numberOne;
-        console.log("Equal: ", numberOne);
-        numberOne = null;
-        displayNumber = "";
-        operationSymbol = "";
-    }
+function resetAll(){
+    location.reload();
 }
